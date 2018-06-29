3cdb60d96df85565ff33ec56a49f512c016fb4a5
Diego Alfonso Serna Rivera <diego.serna@neoris.cxnetworks.net>
2018-06-29 10:27:48 -0500

    Conection Firebase settings environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBf6y51qJPvpFyLEXPJJyzG7IeOjHTH7sQ",
+    authDomain: "mitwitter2-0.firebaseapp.com",
+    databaseURL: "https://mitwitter2-0.firebaseio.com",
+    projectId: "mitwitter2-0",
+    storageBucket: "mitwitter2-0.appspot.com",
+    messagingSenderId: "32860498332"
+  }
 };
