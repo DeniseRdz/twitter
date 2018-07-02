bcce5ff1b77385e504f53b10705cca6fdfcf0b4f
Oscar Armando Recio Delgado <oscar.recio@neoris.cxnetworks.net>
2018-07-02 09:59:23 -0600

    Modal en Home Page

--- src/app/twets.service.ts
+++ src/app/twets.service.ts
@@ -8,28 +8,20 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class TwetsService {
 
   //#region  const
-   dataTableTweets = "tweets";
-   slash = "/"
+   dataTableTweets = 'tweets';
+   slash = '/';
 
 
   //#endregion
 
-  constructor(public angularFireDatabase : AngularFireDatabase) {   }
+  constructor(public angularFireDatabase: AngularFireDatabase) {   }
 
-   getAllTwets(){
+   getAllTwets() {
     return this.angularFireDatabase.list(this.dataTableTweets);
   }
 
-<<<<<<< HEAD
   getTwetsByUserId(usrID) {
     return this.angularFireDatabase.list(this.dataTableTweets);
-=======
-  getTwetsByUserId(){
-     
-    return  this.angularFireDatabase.list(this.dataTableTweets);
-
- 
->>>>>>> 2817c5382c548801f88416cffd7e2b116b46401f
   }
 
 
